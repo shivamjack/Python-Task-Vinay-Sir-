58c97315288d6aeaa73c121d3d9008667f105d01
shivamjack <shivam.yadav8085@gmail.com>
2020-10-12 00:42:01 +0530

    Add files via upload

---
+++ Task 1 (Date 9-10-20).py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1st in built Function
+
+
+# In[12]:
+
+
+A = "hello sir"
+
+C = A.title()# Make the First letter capital of Each case
+
+print(C)
+
+
+# In[3]:
+
+
+#2nd in Built Function
+
+
+# In[10]:
+
+
+jack = "I like CSGO"
+
+print (jack)
+
+
+# In[11]:
+
+
+J = jack.replace("CSGO", "VALORANT") #Replace the Word which is in "want to replace", "Replaceing word"
+
+print(J)
+
+
+# In[8]:
+
+
+#3rd in built Function
+
+
+# In[9]:
+
+
+W = "HOME PAGE"
+
+x = W.lower() #lower case all the words
+
+print(x)
+
+
+# In[13]:
+
+
+#4th in built function
+
+
+# In[14]:
+
+
+S = "1234567890"
+
+A = S.find("7") #Finding the word position in("____")
+
+print(A) 
+
+
+# In[15]:
+
+
+#5th in built Function
+
+
+# In[18]:
+
+
+JK = "4856486186455558641684981465465489446"
+
+U = JK.count("5") # finding the word occurrence how many times it occur
+
+print(U)
+
+
+# In[19]:
+
+
+#6th in built Function
+
+
+# In[20]:
+
+
+val = "JackKnife123"
+
+J = val.isalnum()#checking text are alphanumeric or not 
+
+print(J)
+
+
+# In[21]:
+
+
+#7th in built function
+
+
+# In[33]:
+
+
+B = "JACKKNIFE"
+x = B.lower()#lower case all
+print(x)#
+
+
+# In[23]:
+
+
+#8th in built function
+
+
+# In[24]:
+
+
+UV = "Hello, a, B , C"
+
+K = UV.startswith("Hello")#Check if the string starts with or not
+
+print(K)
+
+
+# In[ ]:
+
+
+
+
---
+++ Task 2 (date 9-10-20).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+str1="welcome to python"
+
+
+# In[8]:
+
+
+"to" in str1
+
+
+# In[9]:
+
+
+"JACK" in str1
+
+
+# In[ ]:
+
+
+
+
