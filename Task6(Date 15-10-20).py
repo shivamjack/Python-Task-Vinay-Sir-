9ac713b9106f04873ce9a2c4b45f4587a81c3bd0
shivamjack <shivam.yadav8085@gmail.com>
2020-10-17 01:28:48 +0530

    Add files via upload

---
+++ Task6(Date 15-10-20).py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+##adding and removing the elements from tuple
+
+
+# In[11]:
+
+
+tup = (1,2,5,3,"JACK")# you cant add or remove from tuple
+
+
+# In[9]:
+
+
+type(tup)
+
+
+# In[10]:
+
+
+lst=list(tup)
+
+
+# In[17]:
+
+
+lst.insert(1,"Python")#I just run this cell 2 times
+
+
+# In[18]:
+
+
+lst= tuple(tup1)
+
+
+# In[19]:
+
+
+tup1# thats why pthyon is showing 2 times  # sry xD
+
+
+# In[28]:
+
+
+t1 =(1,2,3,4,5,2,22,2,)# count the occurance of number
+
+
+# In[29]:
+
+
+t1.count(2)
+
+
+# In[37]:
+
+
+type(t1)
+
+
+# In[ ]:
+
+
+
+
---
+++ Task7(Date15-10-20).py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+#Go through some inbuilt methods
+
+
+# In[18]:
+
+
+# using difference between set A and set B 
+A = {10, 20, 30, 40, 80} 
+B = {100, 30, 80, 40, 60} 
+print (A - B) 
+print (B - A) 
+
+
+# In[21]:
+
+
+A.difference(B)
+
+
+# In[22]:
+
+
+B.difference(A)
+
+
+# In[ ]:
+
+
+#symmetric_difference in built methods
+
+
+# In[23]:
+
+
+A.symmetric_difference(B)
+
+
+# In[ ]:
+
+
+
+
