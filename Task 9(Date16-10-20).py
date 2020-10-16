d3dee6c62c569d0f717c4e21f0458d637093fcee
shivamjack <shivam.yadav8085@gmail.com>
2020-10-17 01:30:12 +0530

    Add files via upload

---
+++ Task 9(Date16-10-20).py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+
+
+num1=int(input("Number first = "))
+num2=int(input('number second = '))
+Jack=int(input('1 for add/(+), 2 for /(-), 3 for multiply/(*), 4 for divide/(/)'))
+
+if (Jack==1):
+    print("add =",num1+num2)
+if (Jack==2):
+    print("subtract =",num1-num2)
+elif (Jack==3):
+    print("Multiply =",num1*num2)
+elif (Jack==4):
+    print("Divide =",num1/num2)
+
+
+# 
+
+# In[ ]:
+
+
+
+
