dbe0686b5306101838e7aa42c4aa0e98062e2fed
shivamjack <shivam.yadav8085@gmail.com>
2020-10-21 16:15:45 +0530

    Add files via upload

---
+++ Task10.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## print number form 10 to 1
+
+# In[3]:
+
+
+count = 10
+while (count>0):
+    print(count)
+    count = count - 1
+
+
+# ## Loops
+
+# In[4]:
+
+
+## useing loop to loop over tuples
+
+
+# In[5]:
+
+
+tuple1 = (0, 1, 2, 3, 'python', 'geek')
+
+
+# In[10]:
+
+
+for items in tuple1:
+    print(items)
+
+
+# In[11]:
+
+
+## useing loop to loop over Dict
+
+
+# In[13]:
+
+
+dic1 ={"bike":"petrol", "car":"Diesel"}
+
+
+# In[14]:
+
+
+for keys in dic1:
+    print(keys)
+
+
+# In[15]:
+
+
+for value in dic1:
+    print(value)
+
+
+# In[16]:
+
+
+## useing loop to loop over sets
+
+
+# In[19]:
+
+
+set1={"Pendrive",1,2,7,3,"Jack"}
+
+
+# In[20]:
+
+
+for items in set1:
+    print(items)
+
+
+# In[21]:
+
+
+## useing loop to loop over lists
+
+
+# In[22]:
+
+
+lst1=["jack",1,2,3,4]
+
+
+# In[23]:
+
+
+for items in lst1:
+    print(items)
+
+
+# In[ ]:
+
+
+
+
